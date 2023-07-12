07ad27bb221ee9fdb47c65404a120d411b91ab8a
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-07-12 17:45:21 +0200

    added js functions

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 
-{const hello = ()=>{
-    console.log("Witam serdecznie zaglądających do konsoli")}
+{
+    const hello = () => {
+        console.log("Witam serdecznie zaglądających do konsoli")
+    }
     hello();
-}
 
-
-let body = document.querySelector(".body");
-let button = document.querySelector(".article__button");
-//przycisk, który przełącz kolory
-let nextColorName = document.querySelector(".article__nextColorName-js");
-//to jest span, w którego innertext się wpisze nazwa 
-
-button.addEventListener("click", () => {
-    //
-    body.classList.toggle("pink");
-    //przełącz klasę jeśli klasa jest pink
-    nextColorName.innerText = body.classList.contains("pink") ? "biały" : "różowy"
-    //spanowi wewnątrz button zmienia tekst-jeżeli zawiera pink
-    //to zmienia na biały a jak nie to różowy
-});
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        let nextColorName = document.querySelector(".article__nextColorName-js");
+        body.classList.toggle("pink");
+        nextColorName.innerText = body.classList.contains("pink") ? "biały" : "różowy"
+    };
+    const init = () => {
+        const button = document.querySelector(".article__button");
+        button.addEventListener("click", toggleBackground);
+    }
+    init();
+};
 
